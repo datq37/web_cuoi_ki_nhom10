a104846da98484ac31f57f164adc92b172196ffd
datco06 <datq280306@gmail.com>
2026-05-19 23:16:32 +0700

    FE: update cấu hình chung và tiện ích

--- config/routes.ts
+++ config/routes.ts
@@ -1,45 +1,37 @@
 export default [
-	{
-		path: '/user',
-		layout: false,
-		routes: [
-			{
-				path: '/user/login',
-				layout: false,
-				name: 'login',
-				component: './user/Login',
-			},
-			{
-				path: '/user',
-				redirect: '/user/login',
-			},
-		],
-	},
-
-	///////////////////////////////////
-	// DEFAULT MENU
-	{
-		path: '/dashboard',
-		name: 'Dashboard',
-		component: './TrangChu',
-		icon: 'HomeOutlined',
-	},
-
-	{
-		path: '/',
-		redirect: '/dashboard',
-	},
-	{
-		path: '/403',
-		component: './exception/403/403Page',
-		layout: false,
-	},
-	{
-		path: '/hold-on',
-		component: './exception/DangCapNhat',
-		layout: false,
-	},
-	{
-		component: './exception/404',
-	},
+  {
+    path: '/',
+    component: './Trang Chủ',
+    layout: false,
+  },
+  {
+    path: '/chinh-sach-bao-mat',
+    component: './Trang Chủ/Component/Chính Sách Bảo Mật',
+    layout: false,
+  },
+  {
+    path: '/dieu-khoan-dich-vu',
+    component: './Trang Chủ/Component/Điều khoản dịch vụ',
+    layout: false,
+  },
+  {
+    path: '/lien-he',
+    component: './Trang Chủ/Component/Liên hệ',
+    layout: false,
+  },
+  {
+    path: '/dang-nhap',
+    component: './Khách Hàng/đăng nhập',
+    layout: false,
+  },
+  {
+    path: '/dang-ky',
+    component: './Khách Hàng/đăng kí',
+    layout: false,
+  },
+  {
+    path: '/trang-chinh',
+    component: './Khách Hàng/Trang chính',
+    layout: false,
+  },
 ];
